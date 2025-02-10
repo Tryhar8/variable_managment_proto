2c3479d5af7431de1142c6356a6909f3e0d3d315
Semir Becic <semir_becic@hotmail.com>
2025-02-11 00:07:45 +0100

    added notifier event to variable model

--- VariableManagementProto/Core/Models/Variable.cs
+++ VariableManagementProto/Core/Models/Variable.cs
@@ -1,12 +1,30 @@
 ﻿namespace Core.Models
 {
+    public delegate void VariableChangeHandler(string identifier, string oldValue, string newValue);
+
     public class Variable
     {
+        private string _value = string.Empty;
+
+        public event VariableChangeHandler VariableChange;
         public int Id { get; init; }
         public string Identifier { get; init; } = string.Empty;
         public int TypeId { get; init; }
         public TypeDefinition Type { get; init; } = null!;
         public string DefaultValue { get; init; } = string.Empty;
-        public string CurrentValue { get; init; } = string.Empty;
+        public string CurrentValue { get => _value; 
+                                     set {
+                                          if(_value != value) 
+                                            {
+                                                VariableChange?.Invoke(Identifier, _value, value);
+                                                _value = value;
+                                            }
+                                         } 
+                                   }
+
+        public void Unsuscribe(VariableChangeHandler handler)
+        {
+            VariableChange -= handler;
+        }
     }
 }
