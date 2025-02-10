daf9f997649a396de92df2973ba09635806eb341
Semir Becic <semir_becic@hotmail.com>
2025-02-10 20:04:24 +0100

    models created

---
+++ VariableManagementProto/Core/Type.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Type
+    {
+        public int? Id { get; init; }
+        public string Name { get; init; } = string.Empty;
+    }
+}
---
+++ VariableManagementProto/Core/Variable.cs
@@ -0,0 +1,12 @@
+﻿namespace Core
+{
+    public class Variable
+    {
+        public int Id { get; init; }
+        public string Identifier { get; init; } = string.Empty;
+        public int TypeId { get; init; }
+        public Type Type { get; init; } = null!;
+        public string DefaultValue { get; init; } = string.Empty;
+        public string CurrentValue {  get; init; } = string.Empty;
+    }
+}
