b37eb8a34e85b9e67f2dbefece634427046b20a6
Semir Becic <semir_becic@hotmail.com>
2025-02-11 21:30:35 +0100

    extension method added

---
+++ VariableManagementProto/Core/ExtensionMethods/VariableExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+
+namespace Core.ExtensionMethods
+{
+    public static class VariableExtensions
+    {
+        public static object ConvertTo(this Variable variable)
+        {
+            ArgumentNullException.ThrowIfNull(variable);
+            ArgumentNullException.ThrowIfNull(variable.Type);
+
+            string typeName = variable.Type.Name?.ToLower()!;
+            
+            ArgumentNullException.ThrowIfNull(typeName);
+
+            string value = variable.CurrentValue;
+
+            return typeName switch
+            {
+                "string" => variable.CurrentValue,
+                "boolean" => bool.TryParse(value, out bool boolResult) ? boolResult
+                          : throw new FormatException($"Cannot convert '{value}' to Boolean."),
+                "integer" => int.TryParse(value, out int intResult) ? intResult
+                          : throw new FormatException($"Cannot convert '{value}' to Integer."),
+                "float" => float.TryParse(value, out float floatResult) ? floatResult
+                              : throw new FormatException($"Cannot convert '{value}' to Float."),
+                "double" => double.TryParse(value, out double doubleResult) ? doubleResult
+                              : throw new FormatException($"Cannot convert '{value}' to Double."),
+                _ => throw new NotSupportedException($"Conversion for type '{variable.Type.Name}' is not supported.")
+            };
+        }
+    }
+}
--- VariableManagementProto/Core/Repositories/IVariableRepository.cs
+++ VariableManagementProto/Core/Repositories/IVariableRepository.cs
@@ -10,6 +10,7 @@ namespace Core.Repositories
     public interface IVariableRepository
     {
         Task<Variable?> GetByIdAsync(int id);
+        Task<Variable?> GetByIdentifier(string identifier);
         Task<IEnumerable<Variable>> GetAllAsync();
         Task AddAsync(Variable variable);
         Task UpdateAsync(Variable variable);
